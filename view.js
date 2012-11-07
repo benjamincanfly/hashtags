1bbb9ebc1f0465bd5b8a9bb5c1d728fe3c52be81
Benjamin <benjamincanfly@gmail.com>
2012-11-06 20:37:36 -0500

    faster

--- view.js
+++ view.js
@@ -3,6 +3,19 @@ var LNJF={
 	
 	format:function(){
 		
+		LNJF.quickFormat();
+		LNJF.slowFormat();
+			
+	},
+	quickFormat:function(){
+		
+		$("#tweets").attr('showusername', $("#formatting input[name=includeusername]").attr('checked')?'yes':'no');
+		$("#tweets").attr('showhash', $("#formatting input[name=includehash]").attr('checked')?'yes':'no');
+		$("#tweets").attr('showatjimmy', $("#formatting input[name=includejimmy]").attr('checked')?'yes':'no');
+		$("#tweets").attr('dofont', $("#formatting input[name=dofont]").attr('checked')?'yes':'no');
+		
+		return true;
+		
 		$(".tweet").each(function(){
 			
 			var tweetID=$(this).attr('tweetid');
@@ -10,24 +23,6 @@ var LNJF={
 			var content="";
 			var tweet=jsonTweets[tweetID];
 			var text=tweet['text'];
-			
-			if(!$("#formatting input[name=includehash]").attr('checked')){
-				text=text.replace(new RegExp('#'+hashtag, 'gim'), '');
-			}
-			
-			if(!$("#formatting input[name=includejimmy]").attr('checked')){
-				text=text.replace(new RegExp('@jimmyfallon', 'gim'), '').replace(new RegExp('@latenightjimmy', 'gim'), '');
-			}
-			
-			if($("#formatting input[name=includeusername]").attr('checked')){
-				text='<a href="http://www.twitter.com/'+tweet['user']+'" class="username">'+tweet['user']+'</a> '+text;
-			}
-			
-			if($("#formatting input[name=dofont]").attr('checked')){
-				$("#tweets").addClass('dofont');
-			} else {
-				$("#tweets").removeClass('dofont');
-			}
 			
 			text=text.replace(/([\s]{1,})/g, ' ');
 			text=$.trim(text);
@@ -37,16 +32,88 @@ var LNJF={
 		});
 		
 	},
-	
+	slowFormat:function(){
+		
+		if($("#formatting input[name=shownumbers]").attr('checked')){
+				$("#tweets").addClass('shownumbers');
+				
+				var divideBy=$("#formatting select[name=divideby]").val();
+				
+				$("#tweets .tweets").each(function(){
+					
+					var numOfThese=$('.tweet', this).size();
+					
+					$(".tweet", this).each(function(i){
+						
+						if(divideBy==2){
+							if(i>=numOfThese/2){
+								$(this).attr('color', 'blue');
+							} else {
+								$(this).attr('color', 'red');
+							}
+						} else if (divideBy==3){
+							if(i>=(numOfThese*(2/3))){
+								$(this).attr('color', 'green');
+							} else if(i>=(numOfThese/3)) {
+								$(this).attr('color', 'blue');
+							} else {
+								$(this).attr('color', 'red');
+							}
+							
+						} else if (divideBy==4){
+							if(i>=numOfThese*.75){
+								$(this).attr('color', 'yellow');
+							} else if(i>=numOfThese/2) {
+								$(this).attr('color', 'green');
+							} else if(i>=numOfThese/4) {
+								$(this).attr('color', 'blue');
+							} else {
+								$(this).attr('color', 'red');
+							}
+						}
+						
+					});
+					
+				});
+				
+				
+			} else {
+				$("#tweets").removeClass('shownumbers');
+				$("#tweets .tweet").attr('color','');
+			}
+			
+	},
+	wrap:function(){
+		$(".tweet").each(function(){
+			
+			var tweetID=$(this).attr('tweetid');
+			
+			var content="";
+			var tweet=jsonTweets[tweetID];
+			var text=tweet['text'];
+			
+			text=text.replace(new RegExp('(#'+hashtag+')([\s]{0,})', 'gim'), "<span class='hash'>\$1 </span>");
+			text=text.replace(new RegExp('(@jimmyfallon)([\s]{0,})', 'gim'), "<span class='atjimmy'>\$1 </span>").replace(new RegExp('(@latenightjimmy)([\s]{0,})', 'gim'), "<span class='atjimmy'>\$1 </span>");
+			text='<a href="http://www.twitter.com/'+tweet['user']+'" class="username">'+tweet['user']+' </a> '+text;
+			
+			//console.info(text);
+			$(this).html(text);
+		});
+		
+	},
 	init:function(){
+		
+		LNJF.wrap();
 		
 		$("#viewControls input").live("click", function(){
 			var rating=$(this).attr('rating');
 			$("#tweets, #viewControls").attr('rating', rating);
 		});
 		
-		$("#formatting input, #formatting select").live("change", function(){
-			LNJF.format();
+		$("#formatting input[name=includehash], #formatting input[name=includejimmy], #formatting input[name=includeusername], #formatting input[name=dofont]").live("change", LNJF.quickFormat);
+		
+		$("#formatting input[name=shownumbers], #formatting select").live("change", function(){
+			LNJF.slowFormat();
 		});
 		
 		LNJF.format();
