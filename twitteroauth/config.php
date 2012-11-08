53d80e6d59c767595bca7d237315fea8e0c88ac2
Benjamin <benjamincanfly@gmail.com>
2012-11-07 23:14:29 -0500

    path fix

--- twitteroauth/config.php
+++ twitteroauth/config.php
@@ -7,4 +7,4 @@
 
 define('CONSUMER_KEY', 'bFeFgMlPCRZcQAd1nqGLA');
 define('CONSUMER_SECRET', 'cG48yQa7DAO1JqfA91uvc2XRwJbLpzWtyeJb7hLKE');
-define('OAUTH_CALLBACK', 'http://localhost/twitteroauth/callback.php');
+define('OAUTH_CALLBACK', 'http://hashtags.benjaminapple.com/twitteroauth/callback.php');
