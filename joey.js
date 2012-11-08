43ce6629a12e150de0fc9312897b60eb593cadf8
Benjamin <benjamincanfly@gmail.com>
2012-11-07 23:09:25 -0500

    perfect

--- joey.js
+++ joey.js
@@ -2,17 +2,16 @@
 $("a#joeyLink").live("click", function(){
 	var matte=$('<div id="joey"><div></div></div>').appendTo('body').on("mousedown", function(){
 		
-		var whichSound=Math.round(Math.random()*22);
-		$("#audio").append("<embed src='/audio/joey/"+whichSound+".mp3' autostart='true' loop='false'/>");
+		$("#audio").append("<embed src='/audio/joey/22.mp3' autostart='true' loop='false'/>");
 		$(this).remove();
 		
 	});
 	
-	var whichSound=Math.round(Math.random()*22);
+	var whichSound=1+Math.round(Math.random()*21);
 	$("#audio").append("<embed src='/audio/joey/"+whichSound+".mp3' autostart='true' loop='false'/>");
 	
 	$("div", matte).on("mousedown", function(){
-		var whichSound=Math.round(Math.random()*22);
+		var whichSound=1+Math.round(Math.random()*21);
 		$("#audio").append("<embed src='/audio/joey/"+whichSound+".mp3' autostart='true' loop='false'/>");
 		return false;
 	});
