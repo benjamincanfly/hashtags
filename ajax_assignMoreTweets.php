482008f6c7bd787a997d9e16cd4ab30227b03fea
Benjamin Apple <benjamin@imac-2a10d4.nbc.com>
2012-12-12 19:14:52 -0500

    remainders

--- ajax_assignMoreTweets.php
+++ ajax_assignMoreTweets.php
@@ -4,9 +4,21 @@
 	
 	require_once("config.php");
 	
-	$qs=sprintf("update tweets set assigned_to_user_id='%s', assignment_time=CURRENT_TIMESTAMP where assigned_to_user_id='0' and hashtag='%s' order by tweet_id ASC limit 100",
+	$qs=sprintf("select count(*) as num from tweets where hashtag='%s' and assigned_to_user_id='%s'", $config['hashtag'], $_SESSION['user_id']);
+	$q=mysql_query($qs);
+	$row=mysql_fetch_assoc($q);
+	$num=$row['num'];
+	
+	$limit=100;
+	
+	if(($num % 100)>0){
+		$limit=100-($num % 100);
+	}         
+	
+	$qs=sprintf("update tweets set assigned_to_user_id='%s', assignment_time=CURRENT_TIMESTAMP where assigned_to_user_id='0' and hashtag='%s' order by tweet_id ASC limit %s",
 		mysql_real_escape_string($_SESSION['user_id']),
-		mysql_real_escape_string($config['hashtag']));
+		mysql_real_escape_string($config['hashtag']),
+		$limit);
 	$q=mysql_query($qs);
 	
 	$response=array();
