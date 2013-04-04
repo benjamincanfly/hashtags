9c043bb443d5c10cdc52cf20c40ad6c12841e174
Benjamin Apple <benjamin@imac-2a10d4.nbc.com>
2013-04-03 20:15:02 -0400

    no error

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
-	error_reporting(E_ALL);
-	ini_set('display_errors', 'On');
+	//error_reporting(E_ALL);
+	//ini_set('display_errors', 'On');
 	
-	//error_reporting(0);
-	//ini_set('display_errors', 'Off');
+	error_reporting(0);
+	ini_set('display_errors', 'Off');
 	
 	session_start();
 	
